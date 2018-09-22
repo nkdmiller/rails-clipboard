53bbbc053fa6694f37778f872882b8dc251bd7a0
Nate Miller <NKDMiller96@gmail.com>
2018-09-21 23:47:09 -0500

    first commit. Generated rails new and generated models/controllers

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,13 @@
+class ProjectsController < ApplicationController
+  def new
+  end
+
+  def show
+  end
+
+  def index
+  end
+
+  def edit
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/edit'
+  get 'projects/new'
+  get 'projects/show'
+  get 'projects/index'
+  get 'projects/edit'
+	root 'welcome#index'
+  get 'welcome/index'
+end
