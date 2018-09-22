f1254ca54d0ac0515b8567963b06b94a75a4918b
Nate Miller <NKDMiller96@gmail.com>
2018-09-22 16:37:40 -0500

    add login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,25 @@ class UsersController < ApplicationController
   def new
   end
 
+  def create
+  	@user = User.create(user_params)
+  	puts @user
+  	puts user_params
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to "/welcome/index"
+    else
+      redirect_to "/users/new"
+    end
+  end
   def edit
+  end
+
+  private
+ 
+  def user_params
+
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'sessions/new'
   post 'sessions/create'
   get 'users/new'
+  post 'users/create'
   get 'users/edit'
   get 'projects/new'
   get 'projects/show'
