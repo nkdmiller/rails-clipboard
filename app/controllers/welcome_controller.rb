93c6e8fd4424eb0eee24b090fcfa22bbbeb0f760
Nate Miller <NKDMiller96@gmail.com>
2018-09-24 17:33:37 -0500

    added collections of tasks and projects to the welcome controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,9 @@ class WelcomeController < ApplicationController
     if session[:user_id].blank?
       redirect_to "/sessions/new"
     else
+    	@user = User.find(current_user.id)
+    	@projects = @user.projects
+    	@tasks = @user.tasks
       render :index
     end
   end
