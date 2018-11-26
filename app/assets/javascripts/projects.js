d6badd7e52ca92d51ab09c21d79690ba1edb294d
Nate Miller <NKDMiller96@gmail.com>
2018-11-25 22:05:34 -0600

    Loadtasks() moved to proper place in asset pipeline

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -10,5 +10,24 @@ $(document).ready(function(){
  		$.get("/products/" + project_id + ".json", function(data) {
  			#rewrite task form to hide table headers and iterate through json for each task. Unhide after button click and send ajax request. 
 	})
+	function loadTasks() {
+		let results = []
+		$("#headers").show()
+		var arr = window.location.toString().split('/')
+		var project_id = arr[arr.length - 1]
+		$.get( "/projects/" + project_id + ".json", function( data ) {
+			data["tasks"].forEach(function(el) {
+			  results.push(el)
+			})
+			var i = 0
+			for (i = 0; i < results.length; i++){
+				if (results[i]["admin"] != true){
+					$("#task-table").append("<%= j render partial: 'projects/task' %>");
+					console.log(results[i])
+					$(".taskName").last().text(results[i]["role"])
+				}
+			}
+		});		
+	}
 });
 
