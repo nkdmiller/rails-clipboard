82b61b3fb1c81cecf6ef18775f8c452c9b51dfb2
Nate Miller <NKDMiller96@gmail.com>
2018-11-27 02:45:03 -0600

    finished adding current projects functionality to welcome index page

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -15,8 +15,8 @@ function Task(filled, admin, username, user_id, role, project_id, id){
 	this.id = id
 }
 
-Task.prototype.setInformation = function() {
-	console.log(this.role)
+Task.prototype.setInformation = function(i="") {
+	console.log()
 	$(".taskName").last().text(this.role)
    	if (this.filled === true && this.user_id != current_id){
 		$(".creator").last().text(this.username)
@@ -29,6 +29,25 @@ Task.prototype.setInformation = function() {
    	else {
 		$(".creator").last().text("Empty!")
 		$(".actionButton").last().append("<h3><a href='/projects/"+this.project_id+"/tasks/" +this.id+ "/edit'><button class='button-green'>Pick Up Task</button></h3>")  		
+   	}
+};
+
+Task.prototype.setInformationForWelcome = function(i,column) {
+	var row = ".task-table"+ i;
+	console.log(this)
+	console.log(row+".taskName"+column)
+	$(row+".taskName"+column).text(this.role)
+   	if (this.filled === true && this.user_id != current_id){
+		$(row+".creator"+column).text(this.username)
+		$(row+".actionButton"+column).text("N/A")   		
+   	}
+   	else if (this.filled === true && this.user_id == current_id){
+		$(row+".creator"+column).text("You!")
+		$(row+".actionButton"+column).append("<h3><a href='/projects/"+this.project_id+"/tasks/" +this.id+ "/edit'><button class='button-red'>Drop Task</button></h3>")
+   	}
+   	else {
+		$(row+".creator"+column).text("Empty!")
+		$(row+".actionButton"+column).append("<h3><a href='/projects/"+this.project_id+"/tasks/" +this.id+ "/edit'><button class='button-green'>Pick Up Task</button></h3>")  		
    	}
 };
 function Project(name, created_at, id){
